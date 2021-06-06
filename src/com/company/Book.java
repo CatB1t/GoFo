328159ff9bb65bb0fb3f8fcf83519910c0898538
dovaaa-emad <77188095+dovaaa-emad@users.noreply.github.com>
2021-06-07 00:00:47 +0200

    first draft class

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,2 +1,13 @@
-package com.company;public class Book {
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Book { //TODO complete
+    private enum BookStatus{Accepted,Denied,Cancelled,Pending}
+    private ArrayList<Integer> slotIndex = new ArrayList<Integer>();
+    private Playground playground;
+    private Player bookedByPlayer;
+    private BookStatus status;
+    private Date bookDate;
 }
--- src/com/company/BookingManager.java
+++ src/com/company/BookingManager.java
@@ -1,2 +1,20 @@
-package com.company;public class BookingManager {
+package com.company;
+
+import java.util.ArrayList;
+
+public class BookingManager { //TODO complete
+    private void updateBookingStatus(Player player,Playground playground, ArrayList<Integer> booking){}; //TODO check parameters
+    private boolean confirmPayment(int amount){ //TODO check return
+
+        return true;
+    };
+    public boolean bookSlots(Player player,Playground playground,ArrayList<Integer> booking){ //TODO check return
+
+        return true;
+    };
+    public boolean cancelBooking(Book booking){ //TODO check return
+
+        return true;
+    };
+
 }
