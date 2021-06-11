1483f0c5e7a220ae66417b887ee9ad849ed597b1
Esraa Yazid <60916510+esraa9322@users.noreply.github.com>
2021-06-11 08:43:42 +0200

    added documentation

--- src/GoFo/Slot.java
+++ src/GoFo/Slot.java
@@ -6,6 +6,12 @@ public class Slot
     public String endTime;
     public String day;
 
+    /**
+     *parametrized constructor sets slot's information.
+     * @param start starting time of the slot.
+     * @param end ending time of the slot.
+     * @param day day of the slot.
+     */
     public Slot(String start, String end, String day)
     {
         this.startTime = start;
@@ -13,6 +19,10 @@ public class Slot
         this.day = day;
     }
 
+    /**
+     * prints slot info.
+     * @return string contains slot information : starting , ending time and the slot's day.
+     */
     @Override
     public String toString()
     {
